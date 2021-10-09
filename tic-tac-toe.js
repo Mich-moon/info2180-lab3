290433a5c3f08df1321b6b66f5effb380b6bc6e5
Michaela Wright <75323745+Mich-moon@users.noreply.github.com>
2021-10-09 03:22:02 -0500

    excercise two

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,7 @@
 
 window.onload = function() {
 
+    // ACTIVITY - ONE
   let board = document.getElementById("board");
   //console.log(board);
   let boxList = board.getElementsByTagName("div");
@@ -10,4 +11,25 @@ window.onload = function() {
     box.setAttribute("class", "square");
   }
   
+   // ACTIVITY - TWO
+  var record = []; // array for storing state
+
+  //Adding click event to all boxes
+  for (let tile of boxList) {
+    tile.onclick = function() {
+     
+        if (record.length == 0 || record[record.length - 1] == "O") {
+            // if array is empty OR if last element is O, put an X and add to state
+            tile.innerHTML = "<strong>X</strong>";
+            tile.classList.add("X");
+            record.push("X");
+
+        } else if (record[record.length - 1] == "X") {
+            // if last element in array is X, then put an O and add to state
+            tile.innerHTML = "<strong>O</strong>";
+            tile.classList.add("O");
+            record.push("O");
+        }
+    }
+  };
 };
